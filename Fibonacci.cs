5b4cff9d6a49a1e85b7ed36125965e8a0680a9db
munteanulucian <49768413+munteanulucian@users.noreply.github.com>
2019-05-03 16:38:53 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Fibonacci
+    {
+        public static void Fib(int count)
+        {
+            int prev = 0;
+            int current = 1;
+            int nr = 1;
+
+            Console.Write(prev + " ");
+            Console.Write(current + " ");
+
+            for (int i = 2; i < count; i++)
+            {
+                nr = prev + current;
+                Console.Write(nr +" ");
+                prev = current;
+                current = nr;
+            }
+        }
+    }
+}
